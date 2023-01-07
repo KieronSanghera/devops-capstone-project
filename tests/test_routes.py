8cabbd13576bcc4e40693b3b6916e89b36399287
KieronSan <kieronsanghera@gmail.com>
2023-01-06 20:10:51 -0500

    refactor

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -163,8 +163,7 @@ class TestAccountService(TestCase):
         updated_account = response.get_json()
         self.assertEqual(updated_account["name"], "Something Known")
     
-    
-
-
-
-    # ADD YOUR TEST CASES HERE ...
+    def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
+        resp = self.client.delete(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
